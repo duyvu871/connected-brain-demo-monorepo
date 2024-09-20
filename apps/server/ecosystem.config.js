9a430f2ceee1dfc0d4ec794aa3e46e696b8b74b4
duyvu871 <zoocp123@gmail.com>
2024-09-21 05:52:30 +0700

    new docker deployment

--- apps/server/ecosystem.config.js
+++ apps/server/ecosystem.config.js
@@ -1,14 +1,18 @@
 module.exports = {
     apps: [
         {
-            name: 'connected-brain',
-            script: 'NODE_ENV=production PORT=3001 npm run start:all',
-            watch: '.',
+            name: 'connected-brain-back-end',
+            script: 'npm run start',
+            out_file: "./back-end-out.log",
+            error_file: "./back-end-error.log",
+            log_date_format: "DD-MM HH:mm:ss Z",
             env: {
                 NODE_ENV: 'development',
+                PORT: 3001
             },
             env_production: {
                 NODE_ENV: 'production',
+                PORT: 3001
             },
         },
     ],
--- apps/web/ecosystem.config.js
+++ apps/web/ecosystem.config.js
@@ -2,13 +2,17 @@ module.exports = {
     apps: [
         {
             name: 'connected-brain-front-end',
-            script: 'NODE_ENV=production PORT=3001 npm run start:all',
-            watch: '.',
+            script: 'npm run start',
+            out_file: "./front-end-out.log",
+            error_file: "./front-end-error.log",
+            log_date_format: "DD-MM HH:mm:ss Z",
             env: {
                 NODE_ENV: 'development',
+                PORT: 3000
             },
             env_production: {
                 NODE_ENV: 'production',
+                PORT: 3000
             },
         },
     ],
