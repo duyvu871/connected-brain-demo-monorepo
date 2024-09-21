60108e2e17372ea0c7437ffe4b6cde82206451ae
duyvu871 <zoocp123@gmail.com>
2024-09-21 08:35:07 +0700

    hidden recognize ui

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,15 @@
+const path = require('node:path');
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
+  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
   experimental: {
+    outputFileTracingRoot: path.resolve(__dirname, "../../"),
     esmExternals: "loose", // required for the canvas to work
   },
   webpack: (config) => {
