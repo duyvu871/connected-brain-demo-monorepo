4377bcb872b2cc5f0ef942cce633a3587e333bcf
duyvu871 <zoocp123@gmail.com>
2025-03-09 14:44:15 +0700

    [update/direct]: add chatbot v1 direct

--- apps/web/src/app/(apps)/app/(chatbot-test)/chatbot-test/page.tsx
+++ apps/web/src/app/(apps)/app/(chatbot-test)/chatbot-test/page.tsx
@@ -2,10 +2,25 @@
 
 import React from 'react';
 import AppChatbot from '@/containers/Apps_v1/Chatbot/AppChatbot';
+import { ToastContainer, type ToastContainerProps } from 'react-toastify';
+import { Toaster } from 'global/contants/defaultComponentProps.ts';
+import NextuiProvider from '@/providers/nextui-provider';
+import { MantineProviderClient } from '@/providers/mantine-provider';
+import { Provider as JotaiProvider } from 'jotai';
+import ReduxProviders from '@/providers/ReduxProviders.tsx';
 
 function Page() {
 	return (
-		<AppChatbot />
+		<NextuiProvider>
+			<MantineProviderClient>
+				<JotaiProvider>
+					<ReduxProviders>
+						<AppChatbot />
+						<ToastContainer {...(Toaster as ToastContainerProps)} />
+					</ReduxProviders>
+				</JotaiProvider>
+			</MantineProviderClient>
+		</NextuiProvider>
 	);
 }
 
